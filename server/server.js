428894c6afd96d38825bb01944386fa8d29ce129
alban <alban@octopuce.fr>
2015-06-25 23:17:47 +0200

    [wip]

--- server/server.js
+++ server/server.js
@@ -1,2 +1,29 @@
 // Let's inject data in the keywords for now
-Keywords.read
+    if( Keywords.find().count() == 0 ){
+        Keywords.insert({name:"OPEN"});
+        Keywords.insert({name:"COWORKING"});
+        Keywords.insert({name:"ENTREPRENEUR"});
+        Keywords.insert({name:"STARTUP"});
+        Keywords.insert({name:"OBJETS"});
+        Keywords.insert({name:"CONNECTES"});
+        Keywords.insert({name:"DATA"});
+        Keywords.insert({name:"PARTICIPATIF"});
+        Keywords.insert({name:"HACKER"});
+        Keywords.insert({name:"TIERS LIEUX"});
+        Keywords.insert({name:"3D"});
+        Keywords.insert({name:"CREATIVE"});
+        Keywords.insert({name:"TELETRAVAIL"});
+        Keywords.insert({name:"INVESTISSEUR"});
+        Keywords.insert({name:"DIGITAL"});
+        Keywords.insert({name:"AGROTECH"});
+        Keywords.insert({name:"ECOSYSTEME"});
+        Keywords.insert({name:"INNOVATION"});
+        Keywords.insert({name:"ADN"});
+        Keywords.insert({name:"FRENCH"});
+        Keywords.insert({name:"COMMUNITY"});
+        Keywords.insert({name:"CROWD FUNDING"});
+        Keywords.insert({name:"ÉCONOMIE "});
+        Keywords.insert({name:"NUMÉRIQUE"});
+        Keywords.insert({name:"MONETIZE"});
+        Keywords.insert({name:"PITCH"});
+    }
